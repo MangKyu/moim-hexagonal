e743bd2a24ea3445113efad4acb40084e62be317
MangKyu <whalsrb1226@gmail.com>
2022-07-10 23:10:30 +0900

    초기 커밋

---
+++ src/main/java/com/mangkyu/moim/hexagonal/MoimHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.moim.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoimHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangkyu/moim/hexagonal/MoimHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangkyu.moim.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
