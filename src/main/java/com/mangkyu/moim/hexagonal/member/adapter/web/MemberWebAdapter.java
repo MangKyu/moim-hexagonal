29bd7de9a2ee23c8d2041694e49a59fe00af48de
MangKyu <whalsrb1226@gmail.com>
2022-07-23 00:03:12 +0900

    구성원 추가를 위한 웹 어댑터 구현

---
+++ src/main/java/com/mangkyu/moim/hexagonal/member/adapter/web/MemberWebAdapter.java
@@ -0,0 +1,27 @@
+package com.mangkyu.moim.hexagonal.member.adapter.web;
+
+import com.mangkyu.moim.hexagonal.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+class MemberWebAdapter {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public ResponseEntity<Void> addMember(@RequestBody @Valid final AddMemberRequest addMemberRequest) {
+        memberService.addMember(addMemberRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/mangkyu/moim/hexagonal/member/application/MemberService.java
@@ -0,0 +1,10 @@
+package com.mangkyu.moim.hexagonal.member.application;
+
+import com.mangkyu.moim.hexagonal.member.adapter.web.AddMemberRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    public void addMember(final AddMemberRequest addMemberRequest) {
+    }
+}
