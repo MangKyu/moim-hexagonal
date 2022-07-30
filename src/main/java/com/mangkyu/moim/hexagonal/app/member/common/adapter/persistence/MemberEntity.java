ed6c1472ca16c19196b5943058e411a492606565
MangKyu <whalsrb1226@gmail.com>
2022-07-30 16:21:12 +0900

    MemberEntity의 loginId에 유니크 인덱스 추가

--- src/main/java/com/mangkyu/moim/hexagonal/app/member/common/adapter/persistence/MemberEntity.java
+++ src/main/java/com/mangkyu/moim/hexagonal/app/member/common/adapter/persistence/MemberEntity.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(indexes = {@Index(name = "idx_member_login_id", columnList = "loginId", unique = true)})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
